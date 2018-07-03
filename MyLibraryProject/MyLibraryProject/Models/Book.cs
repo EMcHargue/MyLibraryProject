aef72014086445b92c675945c8d0922092dc3d5e
EMcHargue <elizabethmchargue@gmail.com>
2018-07-03 18:45:42 -0400

    Added Author and Genre classes

--- MyLibraryProject/MyLibraryProject/Models/Book.cs
+++ MyLibraryProject/MyLibraryProject/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyLibraryProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,10 @@ namespace MyLibrary.Models
     public class Book
     {
         public string Title { get; set; }
-        public string Author { get; set; }
+        public Author Author { get; set; }
+        public Genre[] Genres { get; set; }
         public string Description { get; set; }
-
+        public int YearOfPublication { get; set; }
+        public bool OnShelf { get; set; }
     }
 }
---
+++ MyLibraryProject/MyLibraryProject/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLibraryProject.Models
+{
+    public class Genre
+    {
+        public string NameOfGenre { get; set; }
+    }
+}
