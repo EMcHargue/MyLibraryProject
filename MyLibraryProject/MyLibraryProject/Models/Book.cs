9d1778750ebacb3310a5eb6e030b325fde9a2000
EMcHargue <elizabethmchargue@gmail.com>
2018-07-24 19:40:19 -0400

    Added validation to forms and fixed format issues with buttons

--- MyLibraryProject/MyLibraryProject/Models/Book.cs
+++ MyLibraryProject/MyLibraryProject/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using MyLibraryProject.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,14 @@ namespace MyLibraryProject.Models
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public Author Author { get; set; }
         //public Genre[] Genres { get; set; }
         public string Description { get; set; }
         //public int YearOfPublication { get; set; }
         public bool OnShelf { get; set; }
+        [Range(0,5)]
         public int Rating { get; set; }
     }
 }
